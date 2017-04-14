1f0fc3a001bd62785ff3ada2942db453b717defe
Greg Turmel <greg.turmel@medfusion.com>
2017-04-14 17:06:37 -0400

    push final react product, finish lesson/module 1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import API_KEY from './apikey';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			videos: []
+			videos: [],
+			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
+
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={ this.state.videos }/>
+				<SearchBar
+					onSearchTermChange={videoSearch}
+				/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={ this.state.videos }/>
 			</div>
 		);
 	}
